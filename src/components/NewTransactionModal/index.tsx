0c77f90b1c5670feb23541fb3989ae589982c419
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-20 14:03:26 -0300

    chore: Move createTransaction function from Transactions Table to TransactionsContext

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useContext } from "react";
 import Modal from "react-modal";
+
+import { TransactionsContext } from "../../TransactionsContext";
 
 import { api } from "../../services/api";
 
@@ -17,19 +19,17 @@ interface NewTransactionModalProps {
 Modal.setAppElement('#root');
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
-      title, value, category, type,
-    };
-
-    api.post('transactions', data);
+    createTransaction({ title, amount, category, type });
   }
 
   return (
@@ -58,8 +58,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
         <TransactionTypeContainer>
           <RadioBox
