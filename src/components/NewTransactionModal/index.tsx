4daba6de1e6ff6cf59d496ae38d0ecc863548590
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-20 02:46:07 -0300

    feat: Create and Style New Transaction Modal Form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from "react-modal";
 
+import closeImg from '../../assets/close.svg';
+
 import { Container } from './styles';
 
 interface NewTransactionModalProps {
@@ -17,6 +19,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal"/>
+      </button>
+
       <Container>
         <h2>Cadastrar Transação</h2>
 
